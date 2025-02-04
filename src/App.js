78e25554f41d5f3f3a7f5e6846c16306d3a070e7
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-02-04 22:36:06 +0600

    countsown using pure react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <h3> Count: {count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  )
+}
