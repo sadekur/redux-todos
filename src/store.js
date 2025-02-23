61e8961c564e542f8c273eb55769a055cf7b4729
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-02-23 22:38:10 +0600

    add applies

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import todosReducer from "./services/reducers/todosReducer";
-const store = createStore(todosReducer);
+const store = createStore(todosReducer, applyMiddleware(thunk));
 export default store;
